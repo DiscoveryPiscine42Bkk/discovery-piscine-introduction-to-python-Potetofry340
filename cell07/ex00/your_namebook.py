30381a9814f702ea2bf68f08803c50cc8558ad9a
sirachayadabam <sirachayada@gmail.com>
2025-06-18 16:23:39 +0700

    Update your_namebook.py

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -1,1 +1,17 @@
+def array_of_names(names_dict):
+    full_names = []
+    for first, last in names_dict.items():
+        full_name = first.capitalize() + " " + last.capitalize()
+        full_names.append(full_name)
+    return full_names
 
+
+# Example usage
+persons = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "niel",
+    "fifi": "brindacier"
+}
+
+print(array_of_names(persons))
