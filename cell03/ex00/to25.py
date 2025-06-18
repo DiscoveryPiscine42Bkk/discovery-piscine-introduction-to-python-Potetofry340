34ebecb082474dca466b25e5f565dc2d98bb461c
sirachayadabam <sirachayada@gmail.com>
2025-06-18 11:46:43 +0700

    Update to25.py

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -2,5 +2,5 @@ value = int(input())
 if value >= 25:
   print("Error")
 else:
-  for num is range(value, 25):
-    print(f"Inside the loop, my variable is {num}")
+  for num in range(value, 25):
+    print("Inside the loop, my variable is {num}")
